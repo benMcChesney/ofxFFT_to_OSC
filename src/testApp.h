ba00a2298120c1b1b2252a1c71cf527807ecd7de
Ben McChesney <ben.mcchesney@gmail.com>
2012-11-06 18:36:09 -0800

    Moved everything to FftManager class for easier management.

--- src/testApp.h
+++ src/testApp.h
@@ -3,19 +3,7 @@
 
 
 #include "ofMain.h"
-#include "ofxFft.h"
-#include "ofxOsc.h"
-#include "FftTrigger.h"
-#include "ofxUI.h"
-
-#define MIC 0
-#define NOISE 1
-#define SINE 2
-
-#define HOST "localhost"
-#define PORT 1200
-
-enum Trigger_Mode_t {TM_NONE=0, TM_SETTING, TM_MOVING, TM_NAMING};
+#include "FftManager.h"
 
 
 class testApp : public ofBaseApp{
@@ -28,12 +16,7 @@ public:
 	void update();
 	void draw();
 	
-	void loadSettings();
-	void saveSettings();
 	
-	void drawTriggers();
-	
-	void checkTriggers();
 	
 	void keyPressed  (int key);
 	void keyReleased(int key);
@@ -43,54 +26,10 @@ public:
 	void mouseReleased(int x, int y, int button);
 	void windowResized(int w, int h);
 	
-	// ofxFft related
-	void plot(float* array, int length, float yScale, int xScale, float yOffset);
+	
 	void audioReceived(float* input, int bufferSize, int nChannels);
-	
-	
-	int bufferSize;
-	ofxFft* fft;
-	float* audioInput;
-	float* fftOutput;
-	float* eqFunction;
-	float* eqOutput;
-	float* ifftOutput;
-	
-	// ofxFft related app values
-	int mode;
-	bool useEQ;
-	
-	// OSC related
-	ofxOscSender sender;
-	void handleOSC();
-	void sendSpectrum();
-	void sendTriggers();
-	bool sendFullSpectrum;
-	
-	
-	vector <FftTrigger> triggers;
-	
-	//trigger stuff for drawing.
-	ofPoint oldMouse;
-	bool settingTrigger;
-	Trigger_Mode_t triggerMode;
-	
-	int selTrigger;
     
-    
-    //ofxUI
-    ofxUICanvas *gui;
-	void guiEvent(ofxUIEventArgs &e);
-    
-    void setupOfxUI( ) ;
-    
-    bool bClearXml ;
-    bool bGuiEnabled ;
-    
-    float amplitudeScale ;
-    float triggerDelay ;
-    
-    void createNewTrigger ( float x , float y ) ; 
+    FftManager fftManager ; 
 };
 
 #endif
